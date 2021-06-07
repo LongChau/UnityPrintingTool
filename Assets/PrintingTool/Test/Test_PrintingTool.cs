8143e3f1d1278192e11e35fc236713b6c62dfb02
LongChau1210 <longchau1210@gmail.com>
2021-06-07 13:30:03 +0700

    ~ update

--- Assets/PrintingTool/Test/Test_PrintingTool.cs
+++ Assets/PrintingTool/Test/Test_PrintingTool.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.UI;
 
 public class Test_PrintingTool : MonoBehaviour {
 
@@ -8,14 +11,30 @@ public class Test_PrintingTool : MonoBehaviour {
 
     public PrintingTool printingTool;
 
+    public TMP_InputField printerNameInput;
+    public TMP_InputField imagePathInput;
+    public Button btnPrint;
+
     void Start()
+    {
+        printerNameInput.onEndEdit.AddListener(Handle_printerNameInput_onEndEdit);
+        imagePathInput.onEndEdit.AddListener(Handle_imagePathInput_onEndEdit);
+        btnPrint.onClick.AddListener(Print);
+    }
+
+    private void Handle_imagePathInput_onEndEdit(string input)
+    {
+        filePath = input;
+    }
+
+    private void Handle_printerNameInput_onEndEdit(string input)
+    {
+        printingTool.printerName = input;
+    }
+
+    public void Print()
     {
         printingTool.CmdPrintThreaded(filePath);
         printingTool.StartCheckIsPrintingDone();
-    }
-
-    void Update()
-    {
-        
     }
 }
